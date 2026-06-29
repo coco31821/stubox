13721cfc9192c2309e2374df640dcd8c5da1ae7e
coco31821 <coco318213@gmail.com>
2026-06-29 19:59:44 +0900

    Initial Commit

---
+++ src/main/java/io/coco318213/stubox/StuboxApplication.java
@@ -0,0 +1,13 @@
+package io.coco318213.stubox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StuboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/coco318213/stubox/StuboxApplicationTests.java
@@ -0,0 +1,13 @@
+package io.coco318213.stubox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
